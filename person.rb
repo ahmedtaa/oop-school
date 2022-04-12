012796d6a7ef765b241ff658f9162a6c3e9d27b2
ahmed tawfik ali <a.alrakhawy@gmail.com>
2022-04-12 05:58:38 +0200

    implement decorator pattern

--- person.rb
+++ person.rb
@@ -1,21 +1,37 @@
-class Person
+require './interface'
+
+class Person < Nameable
   attr_accessor :name, :age
   attr_reader :id
 
   def initialize(age, name = 'Unknown', parent_permission: true)
-    @id = Random.rand(1..10_000)
+    super()
+    @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
   end
 
   def can_use_services?
-    @parent_permission || is_of_age?
+    @age >= 18 || @parent_permission
   end
-
-  private
 
   def of_age?
     @age >= 18
   end
+
+  def correct_name
+    @name
+  end
+
+  private :of_age?
 end
+
+person = Person.new(22, 'maximilianus')
+puts person.correct_name
+capitalized_person = CapitalizeDecorator.new(person)
+puts capitalized_person.correct_name
+capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
+puts capitalized_trimmed_person.correct_name
+
+puts person.correct_name
