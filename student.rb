105642b9a21a31fb108a16c4f52b76113c006535
ahmed tawfik ali <a.alrakhawy@gmail.com>
2022-04-14 00:58:09 +0200

    make reviewers notes

--- student.rb
+++ student.rb
@@ -1,5 +1,7 @@
 require './person'
 
+attr_reader :classroom
+
 class Student < Person
   def initialize(age, classroom, name: 'Unknown', parent_permission: true)
     super(age, name, parent_permission: parent_permission)
