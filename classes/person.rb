a139a6ec8cae6ff9226dac1a04a80f0e67b244c9
omarsalem7 <omarsalem0721@gmail.com>
2022-04-19 09:17:34 +0200

    fix linter errors

--- classes/person.rb
+++ classes/person.rb
@@ -12,15 +12,19 @@ class Person < Nameable
     @parent_permission = parent_permission
     @rentals = []
   end
+
   def can_use_services?
     @age >= 18 || @parent_permission
   end
+
   def of_age?
     @age >= 18
   end
+
   def correct_name
     @name
   end
+
   def add_rental(book, date)
     @rentals.push(Rental.new(date, book, self)) unless @rentals.include?(Rental.new(date, book, self))
   end
--- classes/student.rb
+++ classes/student.rb
@@ -7,9 +7,11 @@ class Student < Person
     super(age, name, parent_permission: parent_permission)
     @classroom = nil
   end
+
   def play_hooky
     "¯\(ツ)/¯"
   end
+
   def classroom=(classroom)
     @classroom = classroom
     classroom.student.push(self) unless classroom.student.include?(self)
