368a06ad2ab78fcbaf7fbb44d3ff41a64bc00d58
omarsalem7 <omarsalem0721@gmail.com>
2022-04-20 13:15:25 +0200

    fix linters error

--- classes/person.rb
+++ classes/person.rb
@@ -28,6 +28,6 @@ class Person < Nameable
   def add_rental(book, date)
     @rentals.push(Rental.new(date, book, self)) unless @rentals.include?(Rental.new(date, book, self))
   end
-  
+
   private :of_age?
 end
